0ab96256c8d6cdca0cecd433ca680f7f093c4786
james-gates-0212 <james.gates.0212@gmail.com>
2022-12-27 08:31:46 +0300

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { initializeApp } from 'firebase/app';
 // console.log(process.env.API_KEY)
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
